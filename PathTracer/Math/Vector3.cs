73ee1b4318182c989be65e92dcd58b08f2a294f5
Tahar <tntmeijs@gmail.com>
2020-06-26 18:12:06 +0200

    Vector3 class [ADD] Class for 3D vector arithmetic. [ADD] Unit tests for Vector3 class. [ADD] Unit test project can access internal classes of the path tracer project.

---
+++ PathTracer/Math/Vector3.cs
@@ -0,0 +1,101 @@
+﻿namespace PathTracer.Math
+{
+    /// <summary>
+    /// Basic three-dimensional vector arithmetic
+    /// </summary>
+    class Vector3
+    {
+        /// <summary>
+        /// X component of the vector
+        /// </summary>
+        public double X { get; set; }
+
+        /// <summary>
+        /// Y component of the vector
+        /// </summary>
+        public double Y { get; set; }
+
+        /// <summary>
+        /// Z component of the vector
+        /// </summary>
+        public double Z { get; set; }
+
+        /// <summary>
+        /// Create a new Vector3 with all components set to zero
+        /// </summary>
+        public static Vector3 Zero => new Vector3(0.0d, 0.0d, 0.0d);
+
+        /// <summary>
+        /// Compute the magnitude of this vector
+        /// </summary>
+        /// <returns>Magnitude (a.k.a. length) of this vector</returns>
+        public double Magnitude => System.Math.Sqrt((X * X) + (Y * Y) + (Z * Z));
+
+        /// <summary>
+        /// Create a new Vector3 with all components set to a specified value
+        /// </summary>
+        /// <param name="x">X component of the vector</param>
+        /// <param name="y">Y component of the vector</param>
+        /// <param name="z">Z component of the vector</param>
+        public Vector3(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        /// <summary>
+        /// Inverts all values in the vector
+        /// </summary>
+        /// <param name="A">Input vector</param>
+        /// <returns>New vector with all components set to A *= -1</returns>
+        public static Vector3 operator -(Vector3 A) => new Vector3(-A.X, -A.Y, -A.Z);
+
+        /// <summary>
+        /// Does nothing with a vector, simply keeps all numbers the same
+        /// </summary>
+        /// <param name="A">Input vector</param>
+        /// <returns>Input vector</returns>
+        public static Vector3 operator +(Vector3 A) => A;
+
+        /// <summary>
+        /// Subtracts the two vectors
+        /// </summary>
+        /// <param name="lhs">Input vector on the left hand side</param>
+        /// <param name="rhs">Input evctor on the right hand side</param>
+        /// <returns>Subtracted vector</returns>
+        public static Vector3 operator -(Vector3 lhs, Vector3 rhs) => new Vector3(lhs.X - rhs.X, lhs.Y - rhs.Y, lhs.Z - rhs.Z);
+
+        /// <summary>
+        /// Sums the two vectors
+        /// </summary>
+        /// <param name="lhs">Input vector on the left hand side</param>
+        /// <param name="rhs">Input evctor on the right hand side</param>
+        /// <returns>Summed vector</returns>
+        public static Vector3 operator +(Vector3 lhs, Vector3 rhs) => new Vector3(lhs.X + rhs.X, lhs.Y + rhs.Y, lhs.Z + rhs.Z);
+
+        /// <summary>
+        /// Multiplies the two vectors
+        /// </summary>
+        /// <param name="lhs">Input vector on the left hand side</param>
+        /// <param name="rhs">Input evctor on the right hand side</param>
+        /// <returns>Multiplied vector</returns>
+        public static Vector3 operator *(Vector3 lhs, Vector3 rhs) => new Vector3(lhs.X * rhs.X, lhs.Y * rhs.Y, lhs.Z * rhs.Z);
+
+        /// <summary>
+        /// Calculate the dot product between two vectors
+        /// </summary>
+        /// <param name="A">Input vector A</param>
+        /// <param name="B">Input vector B</param>
+        /// <returns>Dot product between two vectors</returns>
+        public static double Dot(Vector3 A, Vector3 B) => (A.X * B.X) + (A.Y * B.Y) + (A.Z * B.Z);
+
+        /// <summary>
+        /// Calculate the cross product between two vectors
+        /// </summary>
+        /// <param name="A">Input vector A</param>
+        /// <param name="B">Input vector B</param>
+        /// <returns>Cross product between two vectors</returns>
+        public static Vector3 Cross(Vector3 A, Vector3 B) => new Vector3((A.Y * B.Z) - (A.Z * B.Y), (A.Z * B.X) - (A.X * B.Z), (A.X * B.Y) - (A.Y * B.X));
+    }
+}
---
+++ UnitTests/Math/TestVector3.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using PathTracer.Math;
+
+namespace UnitTests.Math
+{
+    [TestClass]
+    public class TestVector3
+    {
+        [TestMethod]
+        public void TestInitializeConstructorVector3()
+        {
+            Vector3 A = new Vector3(1.0d, 2.0d, 3.0d);
+
+            Assert.IsTrue(A.X == 1.0d, "Vector X component did not initialize correctly.");
+            Assert.IsTrue(A.Y == 2.0d, "Vector Y component did not initialize correctly.");
+            Assert.IsTrue(A.Z == 3.0d, "Vector Z component did not initialize correctly.");
+        }
+
+        [TestMethod]
+        public void TestInitializeZeroVector3()
+        {
+            Vector3 A = Vector3.Zero;
+
+            Assert.IsTrue(A.X == 0.0d, "Vector X component did not initialize correctly.");
+            Assert.IsTrue(A.Y == 0.0d, "Vector Y component did not initialize correctly.");
+            Assert.IsTrue(A.Z == 0.0d, "Vector Z component did not initialize correctly.");
+        }
+
+        [TestMethod]
+        public void TestOperatorInvertVector3()
+        {
+            Vector3 A = new Vector3(1.0d, -2.0d, 0.0d);
+            Vector3 inverted = -A;
+
+            Assert.IsTrue(inverted.X == -1.0d, "Vector X component did not invert correctly.");
+            Assert.IsTrue(inverted.Y ==  2.0d, "Vector Y component did not invert correctly.");
+            Assert.IsTrue(inverted.Z ==  0.0d, "Vector Z component did not invert correctly.");
+        }
+
+        [TestMethod]
+        public void TestOperatorNothingVector3()
+        {
+            // Writing +Vector3 will do nothing to the values
+            Vector3 A = new Vector3(1.0d, 2.0d, 3.0d);
+            Vector3 nothing = +A;
+
+            Assert.IsTrue(nothing.X == 1.0d, "Vector X component changed.");
+            Assert.IsTrue(nothing.Y == 2.0d, "Vector Y component changed.");
+            Assert.IsTrue(nothing.Z == 3.0d, "Vector Z component changed.");
+        }
+
+        [TestMethod]
+        public void TestOperatorSubtractVector3()
+        {
+            Vector3 lhs = new Vector3(1.0f, 1.0f,  1.0f);
+            Vector3 rhs = new Vector3(1.0f, 2.0f, -4.0f);
+            Vector3 sub = lhs - rhs;
+
+            Assert.IsTrue(sub.X ==  0.0d, "Vector X component did not subtract correctly.");
+            Assert.IsTrue(sub.Y == -1.0d, "Vector Y component did not subtract correctly.");
+            Assert.IsTrue(sub.Z ==  5.0d, "Vector Z component did not subtract correctly.");
+        }
+
+        [TestMethod]
+        public void TestOperatorAddVector3()
+        {
+            Vector3 lhs = new Vector3(1.0f, 1.0f,  1.0f);
+            Vector3 rhs = new Vector3(1.0f, 2.0f, -4.0f);
+            Vector3 add = lhs + rhs;
+
+            Assert.IsTrue(add.X ==  2.0d, "Vector X component did not add correctly.");
+            Assert.IsTrue(add.Y ==  3.0d, "Vector Y component did not add correctly.");
+            Assert.IsTrue(add.Z == -3.0d, "Vector Z component did not add correctly.");
+        }
+
+        [TestMethod]
+        public void TestOperatorMultiplyVector3()
+        {
+            Vector3 lhs = new Vector3(1.0f, 1.0f, 1.0f);
+            Vector3 rhs = new Vector3(1.0f, 2.0f, -4.0f);
+            Vector3 mtp = lhs * rhs;
+
+            Assert.IsTrue(mtp.X ==  1.0d, "Vector X component did not multiply correctly.");
+            Assert.IsTrue(mtp.Y ==  2.0d, "Vector Y component did not multiply correctly.");
+            Assert.IsTrue(mtp.Z == -4.0d, "Vector Z component did not multiply correctly.");
+        }
+
+        [TestMethod]
+        public void TestDotVector3()
+        {
+            Vector3 A = new Vector3(1.0f, 3.0f, -5.0f);
+            Vector3 B = new Vector3(4.0f, -2.0f, -1.0f);
+            double dot = Vector3.Dot(A, B);
+
+            Assert.IsTrue(dot == 3.0d, "Incorrect dot product result.");
+        }
+
+        [TestMethod]
+        public void TestCrossVector3()
+        {
+            Vector3 A = new Vector3(2.0f, 3.0f, 4.0f);
+            Vector3 B = new Vector3(5.0f, 6.0f, 7.0f);
+            Vector3 cross = Vector3.Cross(A, B);
+
+            Assert.IsTrue(cross.X == -3.0d && cross.Y == 6.0d && cross.Z == -3.0d, "Incorrect cross product result.");
+        }
+
+        [TestMethod]
+        public void TestMagnitudeVector3()
+        {
+            Vector3 A = new Vector3(6.0d, 8.0d, 0.0d);
+
+            Assert.IsTrue(A.Magnitude == 10.0d, "Incorrect vector magnitude.");
+        }
+    }
+}
