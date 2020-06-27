17259f086e4c4734ec1c21e86d0da75f59fbeaf0
Tahar <tntmeijs@gmail.com>
2020-06-27 13:31:18 +0200

    Intersectable primitive interface

---
+++ PathTracer/Primitives/IPrimitiveIntersectTest.cs
@@ -0,0 +1,17 @@
+﻿using PathTracer.Math;
+
+namespace PathTracer.Primitives
+{
+    /// <summary>
+    /// Interface to test ray - primitive intersections
+    /// </summary>
+    interface IPrimitiveIntersectTest
+    {
+        /// <summary>
+        /// Tests whether the ray could intersect with this primitive
+        /// </summary>
+        /// <param name="ray">Ray to test against this primitive</param>
+        /// <returns>True on intersection, false otherwise</returns>
+        bool Intersects(Ray ray);
+    }
+}
