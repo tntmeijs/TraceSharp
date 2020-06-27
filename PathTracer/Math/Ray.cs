913ab80f38c260a1179718fa4bb29c2b544c46b3
Tahar <tntmeijs@gmail.com>
2020-06-27 13:09:20 +0200

    Ray class [ADD] Ray class. [ADD] Ray unit tests.

---
+++ PathTracer/Math/Ray.cs
@@ -0,0 +1,35 @@
+﻿namespace PathTracer.Math
+{
+    class Ray
+    {
+        /// <summary>
+        /// Ray start coordinate
+        /// </summary>
+        public Vector3 Origin { get; }
+
+        /// <summary>
+        /// Direction of the ray
+        /// </summary>
+        public Vector3 Direction { get; }
+
+        /// <summary>
+        /// Create a new ray that starts at (0, 0, 0) points along the positive Z axis (0, 0, 1)
+        /// </summary>
+        public Ray()
+        {
+            Origin = Vector3.Zero;
+            Direction = new Vector3(0.0d, 0.0d, 1.0d);
+        }
+
+        /// <summary>
+        /// Create a new ray that starts at the specified origin and points into the specified direction
+        /// </summary>
+        /// <param name="origin"></param>
+        /// <param name="direction"></param>
+        public Ray(Vector3 origin, Vector3 direction)
+        {
+            Origin      = origin;
+            Direction   = direction.Normalized;
+        }
+    }
+}
---
+++ UnitTests/Math/TestRay.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using PathTracer.Math;
+
+namespace UnitTests.Math
+{
+    [TestClass]
+    public class TestRay
+    {
+        [TestMethod]
+        public void TestRayEmptyConstructor()
+        {
+            Ray ray = new Ray();
+
+            Assert.IsTrue(ray.Origin.Equals(Vector3.Zero),                      "Ray origin incorrect.");
+            Assert.IsTrue(ray.Direction.Equals(new Vector3(0.0d, 0.0d, 1.0d)),  "Ray direction incorrect.");
+        }
+
+        [TestMethod]
+        public void TestRayConstructor()
+        {
+            Ray ray = new Ray(new Vector3(1.0d, 1.0d, 1.0d), new Vector3(0.0d, 0.0d, 4.0d));
+
+            Assert.IsTrue(ray.Origin.Equals(new Vector3(1.0d, 1.0d, 1.0d)),     "Ray origin incorrect.");
+            Assert.IsTrue(ray.Direction.Equals(new Vector3(0.0d, 0.0d, 1.0d)),  "Ray direction incorrect.");
+        }
+    }
+}
