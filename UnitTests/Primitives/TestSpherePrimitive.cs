225e69b2dd2252122197071340c568471bbcbb67
Tahar <tntmeijs@gmail.com>
2020-06-27 15:58:12 +0200

    Fixed sphere primitive tests [CHANGE] Added black materials to all sphere primitives. [CHANGE] Intersections inside a sphere are no longer considered valid.

--- UnitTests/Primitives/TestSpherePrimitive.cs
+++ UnitTests/Primitives/TestSpherePrimitive.cs
@@ -2,6 +2,7 @@
 
 using PathTracer.Math;
 using PathTracer.Primitives;
+using PathTracer.Rendering;
 
 namespace UnitTests.Primitives
 {
@@ -11,7 +12,7 @@ namespace UnitTests.Primitives
         [TestMethod]
         public void TestEmptyConstructorSpherePrimitive()
         {
-            SpherePrimitive sphere = new SpherePrimitive();
+            SpherePrimitive sphere = new SpherePrimitive(new Material(Color.Black, Color.Black));
 
             Assert.IsTrue(sphere.Center.Equals(Vector3.Zero),   "Sphere center is not at (0, 0, 0).");
             Assert.IsTrue(sphere.Radius == 1.0d,                "Sphere radius does not equal 1.");
@@ -20,7 +21,7 @@ namespace UnitTests.Primitives
         [TestMethod]
         public void TestConstructorSpherePrimitive()
         {
-            SpherePrimitive sphere = new SpherePrimitive(new Vector3(1.0d, 0.0d, 5.0d), 0.5d);
+            SpherePrimitive sphere = new SpherePrimitive(new Vector3(1.0d, 0.0d, 5.0d), 0.5d, new Material(Color.Black, Color.Black));
 
             Assert.IsTrue(sphere.Center.Equals(new Vector3(1.0d, 0.0d, 5.0d)),  "Sphere center is not at (1, 0, 5).");
             Assert.IsTrue(sphere.Radius == 0.5d,                                "Sphere radius does not equal 0.5.");
@@ -29,21 +30,21 @@ namespace UnitTests.Primitives
         [TestMethod]
         public void TestRayIntersectInsideSpherePrimitive()
         {
-            SpherePrimitive sphere = new SpherePrimitive();
+            SpherePrimitive sphere = new SpherePrimitive(new Material(Color.Black, Color.Black));
             PrimitiveHitInfo hitInfo;
             Ray ray = new Ray();
 
             // Test if the ray hits the sphere when the ray starts inside the sphere
             bool hit = sphere.TestRayIntersection(ray, 0.1d, 10000.0d, out hitInfo);
 
-            Assert.IsTrue(hit,                                  "Ray starts inside the sphere, this should be a hit.");
+            Assert.IsFalse(hit,                                 "Ray starts inside the sphere, this should not be a hit.");
             Assert.IsTrue(hitInfo.Distance == sphere.Radius,    "Hit distance should equal the sphere's radius.");
         }
 
         [TestMethod]
         public void TestRayIntersectInFrontSpherePrimitive()
         {
-            SpherePrimitive sphere = new SpherePrimitive();
+            SpherePrimitive sphere = new SpherePrimitive(new Material(Color.Black, Color.Black));
             PrimitiveHitInfo hitInfo;
             Ray ray = new Ray(new Vector3(0.0d, 0.0d, -5.0d), Vector3.Forward);
 
@@ -57,7 +58,7 @@ namespace UnitTests.Primitives
         [TestMethod]
         public void TestRayIntersectBehindSpherePrimitive()
         {
-            SpherePrimitive sphere = new SpherePrimitive();
+            SpherePrimitive sphere = new SpherePrimitive(new Material(Color.Black, Color.Black));
             PrimitiveHitInfo hitInfo;
             Ray ray = new Ray(new Vector3(0.0d, 0.0d, 5.0d), Vector3.Forward);
 
@@ -73,7 +74,7 @@ namespace UnitTests.Primitives
             const double NEAR   = 0.1d;
             const double FAR    = 10000.0d;
 
-            SpherePrimitive sphere = new SpherePrimitive();
+            SpherePrimitive sphere = new SpherePrimitive(new Material(Color.Black, Color.Black));
             PrimitiveHitInfo hitInfo;
 
             // Far out of bounds (miss)
